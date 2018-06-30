5b3f719a924654f5bea4e574acbdd03a675d24ae
davidmueller13 <david.mueller1013@gmail.com>
2018-06-30 20:23:17 +0200

    fix derps
    Change-Id: I34d76e14001e3d951ed1c8841e53e71d83f23e34

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -595,7 +595,6 @@ static long msm_private_ioctl(struct file *file, void *fh,
 	switch (cmd) {
 	case MSM_CAM_V4L2_IOCTL_NOTIFY:
 	case MSM_CAM_V4L2_IOCTL_CMD_ACK:
-	case MSM_CAM_V4L2_IOCTL_NOTIFY_DEBUG:
 	case MSM_CAM_V4L2_IOCTL_NOTIFY_ERROR:
 		break;
 	default:
